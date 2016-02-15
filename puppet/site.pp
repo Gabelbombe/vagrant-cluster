ffd1894ebc86d012d7acaf392f256fe6c4eb2489
ehime <dodomeki@gmail.com>
2016-02-14 17:57:00 -0800

    Site pp contained dupe

--- puppet/site.pp
+++ puppet/site.pp
@@ -5,7 +5,7 @@ node default {
   include ntp, git
 }
 
-node 'node01.mheducation.com', 'node02.mheducation.com', 'node02.mheducation.com' {
+node 'node01.mheducation.com', 'node02.mheducation.com', 'node03.mheducation.com' {
   # Test message
   notify { "Debug output on ${hostname} node.": }
 
