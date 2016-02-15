aac912e13de8af8275a96576e018fdbf0862d890
ehime <dodomeki@gmail.com>
2016-02-14 17:57:58 -0800

    removing docker and fig

--- puppet/site.pp
+++ puppet/site.pp
@@ -9,5 +9,5 @@ node 'node01.mheducation.com', 'node02.mheducation.com', 'node03.mheducation.com
   # Test message
   notify { "Debug output on ${hostname} node.": }
 
-  include ntp, git, docker, fig
+  include ntp, git
 }
