5848a06e75b34332315e17092faa04d8bb284c4d
ehime <dodomeki@gmail.com>
2016-02-15 10:39:34 -0800

    Adding runonce to scriptsdir for master

--- shell/master.sh
+++ shell/master.sh
@@ -38,23 +38,3 @@ fi
 
 # Add mhedu to autosigning
 #echo '*.mheducation.com' >> /etc/puppet/autosign.conf
-
-echo '
-# Start Puppet master
-sudo service puppetmaster status  # test that puppet master was installed
-sudo service puppetmaster stop
-
-# Ctrl+C to kill puppet master
-sudo puppet master --verbose --no-daemonize &
-PID=$! ; sleep 10 ; sudo kill $PID
-
-sudo service puppetmaster start
-sudo puppet cert list --all       # check for `puppet` cert
-
-ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
-rm -f $ABSOLUTE_PATH
-' >> ~/runonce.sh
-
-chmod a+x ~/runonce.sh
-
-echo ~/.bashrc > '[ -f ~/runonce.sh ] && bash ~/runonce.sh'
