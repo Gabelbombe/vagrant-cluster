4d68262b5185228534c4d0cc79e73605ff7d2c28
ehime <dodomeki@gmail.com>
2016-02-14 18:09:08 -0800

    Adding HAProxy node

--- puppet/site.pp
+++ puppet/site.pp
@@ -11,3 +11,10 @@ node 'node01.mheducation.com', 'node02.mheducation.com', 'node03.mheducation.com
 
   include ntp, git
 }
+
+node 'haproxy.mheducation.com' {
+  # Test message
+  notify { "Debug output on proxy ${hostname} node.": }
+
+    include ntp, git
+}
