b77eb0fdeef6f938f78e2f2712ea10822c4db4b3
Michael Osburn <michael.osburn@mheducation.com>
2016-03-01 12:14:25 -0800

    removing haproxy

--- puppet/site.pp
+++ puppet/site.pp
@@ -12,32 +12,3 @@ node 'node01.mosburn.com', 'node02.mosburn.com', 'node03.mosburn.com' {
   include ntp, git
 }
 
-node 'haproxy.mosburn.com' {
-  # Test message
-  notify { "Debug output on proxy ${hostname} node.": }
-
-  include ntp, git, haproxy
-
-  class haproxy_node_config () inherits haproxy {
-    haproxy::listen { 'puppet00':
-      collect_exported => false,
-      ipaddress        => '*',
-      ports            => '80',
-      mode             => 'http',
-      options          => {
-        'option'  => ['httplog'],
-        'balance' => 'roundrobin',
-      },
-    }
-
-    Haproxy::Balancermember <<| listening_service == 'puppet00' |>>
-
-    haproxy::balancermember { 'haproxy':
-      listening_service => 'puppet00',
-      server_names      => ['node01.mosburn.com', 'node02.mosburn.com', 'node03.mosburn.com'],
-      ipaddresses       => ['192.168.32.10',          '192.168.32.20',          '192.168.32.30'],
-      ports             => '80',
-      options           => 'check',
-    }
-  }
-}
