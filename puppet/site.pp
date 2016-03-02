a20c2e715c15cecc57a443980d95b23bf0b92cc3
Michael Osburn <michael.osburn@mheducation.com>
2016-03-02 09:54:03 -0800

    adding some puppet modules

--- puppet/site.pp
+++ puppet/site.pp
@@ -2,13 +2,12 @@ node default {
   # Test message
   notify { "Debug output on ${hostname} node.": }
 
-  include ntp, git
+  include ntp, git, mosburn-editors, centos7utils, epel
 }
 
 node 'node01.mosburn.com', 'node02.mosburn.com', 'node03.mosburn.com' {
   # Test message
   notify { "Debug output on ${hostname} node.": }
 
-  include ntp, git
+  include ntp, git, mosburn-editors, centos7utils, epel
 }
-
